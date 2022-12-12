91fd1f881cafc2cb3243fd0387adac68a737f757
mine9607 <81383389+mine9607@users.noreply.github.com>
2022-12-11 20:35:06 -0500

    wrote code to solve exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, times) {
+    let repeated = ""
+    if (times < 0) {
+        return "ERROR"
+    } else {
+        for (let i = 0; i < times; i++) {
+            repeated += str
+        }
+    }
+    return repeated;
+}
 
-};
+//repeatString('hey',3)
 
 // Do not edit below this line
 module.exports = repeatString;

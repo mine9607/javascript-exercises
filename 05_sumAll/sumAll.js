a49ff61ca21c6dca73116542c51d0b9a8ac3b5be
mine9607 <81383389+mine9607@users.noreply.github.com>
2022-12-12 03:39:20 -0500

    wrote code to solve sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    let sums = 0
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    if(num1 < 0 || num2 < 0){
+        return "ERROR"
+    } else if(num1 < num2){
+        for(let i = num1; i<=num2; i++){
+            sums += i
+        }
+    } else {
+        for(let i = num2; i<=num1; i++)
+            sums += i
+    }
+    return sums
 };
 
 // Do not edit below this line

a974b690d6bf4e345bdb0c63de6df5fffc9e3bd9
mine9607 <81383389+mine9607@users.noreply.github.com>
2022-12-12 05:06:45 -0500

    added code to convert temp between C and F

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let temp = ((num-32)*(5/9))
+  return Number(temp.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let temp = (num*(9/5)+32)
+  return Number(temp.toFixed(1))
 };
 
 // Do not edit below this line

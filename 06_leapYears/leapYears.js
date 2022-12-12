fd8421722a4c0a1e7b23f1ca25245509be6eaf46
mine9607 <81383389+mine9607@users.noreply.github.com>
2022-12-12 04:56:07 -0500

    added code to predict leapyears for exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(number) {
+    if((number%4===0 && number%100!==0)||(number%400===0 && number%100===0)){
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
